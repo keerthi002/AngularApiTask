607370d9c11bc32ae4addf97e74dd9c60c0b4cee
keerthi002 <121797346+keerthi002@users.noreply.github.com>
2023-01-12 20:11:50 +0530

    nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
        
     ]
 })
