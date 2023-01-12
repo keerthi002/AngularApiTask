012d7e931d7501a1e7066a1d02d432273778b148
keerthi002 <121797346+keerthi002@users.noreply.github.com>
2023-01-12 19:15:54 +0530

    re,moved btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserIPComponent } from "./components/user-ip/user-ip.component";
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        UserIPComponent,
+       
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        UserIPComponent,
+       
     ]
 })
 export class AppModule { }
